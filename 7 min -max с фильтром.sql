33de0bd797bdbacedc459f560bdd9c0ddf511162
SpaceGelato <89966588+SpaceGelato@users.noreply.github.com>
2022-08-29 20:50:20 +0300

    Add files via upload

---
+++ 7 min -max с фильтром.sql
@@ -0,0 +1,8 @@
+--Составьте таблицу, в которую войдёт дата проведения раунда, а также минимальное и максимальное значения суммы инвестиций, привлечённых в эту дату.
+--Оставьте в итоговой таблице только те записи, в которых минимальное значение суммы инвестиций не равно нулю и не равно максимальному значению
+
+
+SELECT funded_at, MIN(raised_amount), MAX(raised_amount)
+FROM funding_round
+GROUP BY funded_at
+HAVING MIN(raised_amount) != 0 AND MIN(raised_amount) !=MAX(raised_amount)
